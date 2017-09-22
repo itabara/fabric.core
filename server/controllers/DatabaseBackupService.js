26364158ecc61960539f03a10b796228463dd185
lawrika <laurentiuh@gmail.com>
2017-09-22 06:34:11 +0000

    Generated code by SwaggerHub

---
+++ server/controllers/DatabaseBackupService.js
@@ -0,0 +1,86 @@
+'use strict';
+
+exports.provisioningHostingHosting_nameDatabaseDatabase_nameBackupsGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * hosting_name (String)
+  * database_name (String)
+  **/
+    var examples = {};
+  examples['application/json'] = [ {
+  "name" : "aeiou",
+  "type" : "aeiou",
+  "status" : "aeiou",
+  "completed_date" : "2000-01-23T04:56:07.000+00:00"
+} ];
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.provisioningHostingHosting_nameDatabaseDatabase_nameBackupsPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * hosting_name (String)
+  * database_name (String)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "_utid" : "d290f1ee-6c54-4b01-90e6-d701748f0851",
+  "status" : "pending"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.provisioningHostingHosting_nameDatabaseDatabase_nameRestoresGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * hosting_name (String)
+  * database_name (String)
+  **/
+    var examples = {};
+  examples['application/json'] = [ "" ];
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.provisioningHostingHosting_nameDatabaseDatabase_nameRestoresPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * hosting_name (String)
+  * database_name (String)
+  * user (String)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "_utid" : "d290f1ee-6c54-4b01-90e6-d701748f0851",
+  "status" : "pending"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
---
+++ server/controllers/FTPService.js
@@ -0,0 +1,113 @@
+'use strict';
+
+exports.provisioningHostingHosting_nameFtpGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * hosting_name (String)
+  **/
+    var examples = {};
+  examples['application/json'] = [ {
+  "user_id" : "aeiou",
+  "quota" : 200,
+  "ftp_path" : "/www-data",
+  "username" : "admin@example.com"
+} ];
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.provisioningHostingHosting_nameFtpPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * hosting_name (String)
+  * ftp_account (FtpUserInfo)
+  **/
+    var examples = {};
+  examples['application/json'] = [ {
+  "user_id" : "aeiou",
+  "quota" : 200,
+  "ftp_path" : "/www-data",
+  "username" : "admin@example.com"
+} ];
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.provisioningHostingHosting_nameFtpUsernameDELETE = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * hosting_name (String)
+  * username (String)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "_utid" : "d290f1ee-6c54-4b01-90e6-d701748f0851",
+  "status" : "pending"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.provisioningHostingHosting_nameFtpUsernameGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * hosting_name (String)
+  * username (String)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "user_id" : "aeiou",
+  "quota" : 200,
+  "ftp_path" : "/www-data",
+  "username" : "admin@example.com"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.provisioningHostingHosting_nameFtpUsernamePUT = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * hosting_name (String)
+  * username (String)
+  * ftp_account (FtpUserInfo)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "_utid" : "d290f1ee-6c54-4b01-90e6-d701748f0851",
+  "status" : "pending"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
